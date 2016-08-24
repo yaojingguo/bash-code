e41bfca10936d9a5f0b05a74ef19c464b7aa9cb6
Jingguo Yao <yaojingguo@gmail.com>
2016-08-24 11:28:59 +0800

    Update

--- let.sh
+++ let.sh
@@ -8,3 +8,19 @@ let "num = (( 0 && 11))"
 echo $?
 
 echo "Logical OR"
+((200 || 11))
+echo $?
+let "num = ((200 || 11))"
+echo $num
+let "num = ((200 || 11))"
+echo $?
+
+echo "Bitwise OR"
+((2 | 1))
+echo $?
+let "num = ((2 | 1))"
+echo $num
+let "num = ((2 | 11))"
+echo $?
+
+
