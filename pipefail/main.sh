32bd9d686ab251962686d4c63b317c5e6d59a80e
Jingguo Yao <yaojingguo@gmail.com>
2016-09-18 10:49:41 +0800

    Update

--- pipefail/main.sh
+++ pipefail/main.sh
@@ -5,30 +5,11 @@ set -euo pipefail
 # ! ./tool.sh
 # echo "FINISH"
 
-Redirect() {
+TestVet() {
   local vet=$(./tool.sh 2>&1)
   ! echo ":$vet" | grep -vE 'xxxx'
 }
-# Redirect
 
-# TestVet() {
-#   # solution 1
-#   # ! { ! ./tool.sh; }  |& grep -vE 'xxxx' 
-#   # solution 2
-#   # ! { ./tool.sh 2>&1 || true; } | grep -vE 'xxxx' 
-#   # solution 3
-#   # vet=$(./tool.sh 2>&1)
-#   vet=`./tool.sh`
-#   echo $vet
-#   # ! echo $vet | grep -vE 'xxxx'
-# }
-# TestVet
-# output=$(eval 'Redirect') || exit_status=$?
-# echo "exit_status: $exit_status"
-# echo "output: $output"
-
-
-export -f 'Redirect' 
 echo "----------------"
 output=$(eval 'Redirect')
 echo "exit_status: $?"
