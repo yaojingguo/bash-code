944393288265e2fb0788c8d4a409070441fe1081
Jingguo Yao <yaojingguo@gmail.com>
2016-09-18 10:37:10 +0800

    Update

--- pipefail/main.sh
+++ pipefail/main.sh
@@ -2,14 +2,28 @@
 
 set -euo pipefail
 
-TestVet() {
-  ! { ! ./tool.sh; }  |& grep -vE 'xxxx' 
-  # ! { ./tool.sh 2>&1 || true; } | grep -vE 'xxxx' 
-  # vet=$(./tool.sh || true)
-  # ! echo $vet | grep -vE 'xxxx'
-}
+# ! ./tool.sh
+# echo "FINISH"
 
-# TestVet
-output=$(eval 'TestVet') || exit_status=$?
-echo "exit_status: $exit_status"
-echo "output: $output"
+Redirect() {
+  local vet=$(./tool.sh 1>&2)
+  echo $vet
+}
+Redirect
+
+# TestVet() {
+#   # solution 1
+#   # ! { ! ./tool.sh; }  |& grep -vE 'xxxx' 
+#   # solution 2
+#   # ! { ./tool.sh 2>&1 || true; } | grep -vE 'xxxx' 
+#   # solution 3
+#   # vet=$(./tool.sh 2>&1)
+#   vet=`./tool.sh`
+#   echo $vet
+#   # ! echo $vet | grep -vE 'xxxx'
+# }
+
+# # TestVet
+# output=$(eval 'TestVet') || exit_status=$?
+# echo "exit_status: $exit_status"
+# echo "output: $output"
